79b7fd87be68fa18a8bae38c6c713a63ed6fef6b
Gilang Shalahuddin Purnama <justmegilang@gmail.com>
2016-05-16 15:40:40 +0700

    Add map as component (not finished)

--- app/component/heritago-map/heritago-map.js
+++ app/component/heritago-map/heritago-map.js
@@ -11,7 +11,38 @@ var HeritagoMap = Izzel.Component.extend({
         var text = this.$el.children('input').val();
         window.izzel.dispatcher.trigger('searchbar:change', this.el, text);
         console.log('[EVENT] searchbar:change of ', this.el, ' <', text, '>');
-    }
+    },
+    
+    
+    mapOption:{
+        center:undefined,
+        zoom: 12,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    },
+    
+    map: undefined,
+    
+    showPosition: function (location) {
+        console.log(location);
+    
+        var currentLocation = new google.maps.LatLng(location.coords.latitude, location.coords.longitude);
+            
+        this.mapOption.center = currentLocation;
+        
+        this.map = new google.maps.Map(document.getElementById("map-canvas"),this.mapOption);
+    
+        
+        var marker = new google.maps.Marker({
+            position: currentLocation,
+            map: this.map
+        });
+            
+        service = new google.maps.places.PlacesService(this.map);
+        google.maps.event.addListenerOnce(this.map, 'bounds_changed', search);
+            
+        $('#refresh').click(search);
+
+    },
 });
 
 module.exports = HeritagoMap;
